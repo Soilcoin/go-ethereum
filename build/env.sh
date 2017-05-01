be511f2e741258ba588daa04383b5132f5fab44b
Marty McFly <martymcfly2016@protonmail.com>
2017-05-01 22:48:11 +0200

    geändert:       build/env.sh

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src/github.com/Soilcoin"
+if [ ! -L "$ethdir/go-soil" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. go-soil
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/go-soil"
+PWD="$ethdir/go-soil"
 
 # Launch the arguments with the configured environment.
 exec "$@"
