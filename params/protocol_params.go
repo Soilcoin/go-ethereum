a81621c5d32fdb8edfb5de3059b44b7bb9e5e211
Marty McFly <martymcfly2016@protonmail.com>
2017-05-01 23:06:04 +0200

    Modify protocol params

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -68,7 +68,12 @@ var (
 	MinGasLimit            = big.NewInt(5000)                  // Minimum the gas limit may ever be.
 	GenesisGasLimit        = big.NewInt(4712388)               // Gas limit of the Genesis block.
 	TargetGasLimit         = new(big.Int).Set(GenesisGasLimit) // The artificial target
-	DifficultyBoundDivisor = big.NewInt(2048)                  // The bound divisor of the difficulty, used in the update calculations.
+	//DifficultyBoundDivisor = big.NewInt(2048)                // The bound divisor of the difficulty, used in the update calculations.
+	DifficultyBoundDivisor = big.NewInt(1024)
+	HardFork1              = big.NewInt(263708)	               // Hard fork block reward 8 to 4 SOIL
+	HardFork2              = big.NewInt(320000)                // Hard fork block time to 60 seconds
+	HardFork3              = big.NewInt(334000)                // Hard fork block time to back to 15 seconds and block reward to 1 SOIL
+	HardFork4              = big.NewInt(2000100)               // block reward to 3 SOIL		
 	GenesisDifficulty      = big.NewInt(131072)                // Difficulty of the Genesis block.
 	MinimumDifficulty      = big.NewInt(131072)                // The minimum that the difficulty may ever be.
 	DurationLimit          = big.NewInt(13)                    // The decision boundary on the blocktime duration used to determine whether difficulty should go up or not.
