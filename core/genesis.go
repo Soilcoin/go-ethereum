efbb3a41ed31aaac2166b5a3d219b0eb50c5f4c8
Marty McFly <martymcfly2016@protonmail.com>
2017-05-01 23:33:09 +0200

    Modify DefaultGenesisBlock

--- core/genesis.go
+++ core/genesis.go
@@ -258,10 +258,10 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
-		Nonce:      66,
-		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
+		Nonce:      35,
+		ExtraData:  hexutil.MustDecode("0xce02dec31ca49f3c8f149b3b931a0155121d2ca0"),
 		GasLimit:   5000,
-		Difficulty: big.NewInt(17179869184),
+		Difficulty: big.NewInt(1073741824),
 		Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
