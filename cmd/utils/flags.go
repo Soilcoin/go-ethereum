a021be29a80639970f4273a20ab60d3214bbe9e2
Marty McFly <martymcfly2016@protonmail.com>
2017-05-02 15:20:01 +0200

    ListenPortFlag back to 30303

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -352,7 +352,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 39420,
+		Value: 30303,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
